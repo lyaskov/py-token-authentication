7859d53af98ccd44dc3931584bdc3402d7383d8b
lyaskov <lyaskov1@gmail.com>
2025-04-17 20:03:08 +0300

    Refactor OrderViewSet to move queryset logic into get_queryset()

--- cinema/views.py
+++ cinema/views.py
@@ -145,16 +145,16 @@ class OrderPagination(PageNumberPagination):
 class OrderViewSet(mixins.CreateModelMixin,
                    mixins.ListModelMixin,
                    GenericViewSet):
-    queryset = Order.objects.prefetch_related(
-        "tickets__movie_session__movie", "tickets__movie_session__cinema_hall"
-    )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        return Order.objects.filter(user=self.request.user).prefetch_related(
+            "tickets__movie_session__movie",
+            "tickets__movie_session__cinema_hall"
+        )
 
     def get_serializer_class(self):
         if self.action == "list":
